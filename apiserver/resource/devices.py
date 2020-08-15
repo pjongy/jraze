26aef3d2dd2f8a7de9a70bdacf55de10d05a80ae
pjongy <whdduq218@gmail.com>
2020-08-15 16:25:19 +0900

    Implement device searching resource

--- apiserver/resource/devices.py
+++ apiserver/resource/devices.py
@@ -6,13 +6,14 @@ import deserialize
 from apiserver.decorator.request import request_error_handler
 from apiserver.repository.device import find_device_by_device_id, device_model_to_dict, \
     create_device, update_device, DevicePropertyBridge, add_device_properties, \
-    device_property_model_to_dict, remove_device_properties
+    device_property_model_to_dict, remove_device_properties, search_devices
 from apiserver.repository.device_notification_event import find_notification_events_by_device_id, \
     device_notification_event_model_to_dict
 from apiserver.resource import json_response, convert_request
 from common.logger.logger import get_logger
 from common.model.device import DevicePlatform, SendPlatform
 from common.model.device_notification_event import Event
+from common.structure.condition import ConditionClause
 
 logger = get_logger(__name__)
 
@@ -59,12 +60,27 @@ class FetchDeviceNotificationEventsRequest:
     order_bys: List[str]
 
 
+@deserialize.parser('start', int)
+@deserialize.parser('size', int)
+@deserialize.default('device_ids', [])
+@deserialize.default('conditions', {})
+@deserialize.default('order_bys', [])
+@deserialize.parser('order_bys', lambda arg: arg.split(','))  # comma separated string to list
+class SearchDevicesRequest:
+    device_ids: List[str]
+    conditions: dict
+    start: int
+    size: int
+    order_bys: List[str]
+
+
 class DevicesHttpResource:
     def __init__(self, router, storage, secret, external):
         self.router = router
 
     def route(self):
         self.router.add_route('POST', '', self.create_device)
+        self.router.add_route('POST', ':search', self.search_devices)
         self.router.add_route('GET', '/{device_id}', self.get_device)
         self.router.add_route('PUT', '/{device_id}', self.update_device)
         self.router.add_route('DELETE', '/{device_id}/properties', self.delete_properties)
@@ -84,6 +100,32 @@ class DevicesHttpResource:
             return json_response(reason=f'invalid device_id {device_id}', status=404)
 
         return json_response(result=device_model_to_dict(row=device))
+
+    @request_error_handler
+    async def search_devices(self, request):
+        request: SearchDevicesRequest = convert_request(SearchDevicesRequest, await request.json())
+
+        try:
+            conditions: ConditionClause = deserialize.deserialize(
+                ConditionClause, request.conditions)
+        except deserialize.exceptions.DeserializeException as error:
+            return json_response(reason=f'wrong condition clause {error}', status=400)
+
+        total, devices = await search_devices(
+            device_ids=request.device_ids,
+            conditions=conditions,
+            start=request.start,
+            size=request.size,
+            order_bys=request.order_bys,
+        )
+
+        return json_response(result={
+            'total': total,
+            'devices': [
+                device_model_to_dict(device)
+                for device in devices
+            ]
+        })
 
     @request_error_handler
     async def create_device(self, request):
