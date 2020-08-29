8616deb54d5d14639f35f0e4f2d1f8d67b62581e
pjongy <whdduq218@gmail.com>
2020-08-29 22:51:00 +0900

    Implement typed comparison

--- apiserver/repository/device.py
+++ apiserver/repository/device.py
@@ -146,14 +146,32 @@ async def remove_device_properties(
 
 
 def _resolve_condition_clause_to_q(condition_clause: ConditionClause):
+    available_join_types = {'OR', 'AND'}
+    VALUE_INT = 'device_properties__value_int'
+    VALUE_STR = 'device_properties__value_str'
+    operators = {
+        'int_eq': VALUE_INT,
+        'int_gt': f'{VALUE_INT}__gt',
+        'int_gte': f'{VALUE_INT}__gte',
+        'int_lt': f'{VALUE_INT}__lt',
+        'int_lte': f'{VALUE_INT}__lte',
+        'str_exists': f'{VALUE_STR}__contains',
+        'str_eq': VALUE_STR,
+    }
+
     if condition_clause.conditions is not None:
+        if condition_clause.join_type not in available_join_types:
+            raise ValueError('Unknown join_type in ConditionClause')
         repeatable_clause = []
         for _condition_clause in condition_clause.conditions:
             repeatable_clause.append(_resolve_condition_clause_to_q(_condition_clause))
         return Q(*repeatable_clause, join_type=condition_clause.join_type)
+    if condition_clause.operator not in operators:
+        raise ValueError('Unknown operator in ConditionClause')
+
     parameter = {
         'device_properties__key': condition_clause.key,
-        'device_properties__value': condition_clause.value
+        operators[condition_clause.operator]: condition_clause.value
     }
     return Q(**parameter)
 
@@ -187,9 +205,7 @@ async def search_devices(
     size: int = 10,
     order_bys: List[str] = (),
 ) -> Tuple[int, List[Device]]:
-    filter_ = Q()
-    if conditions.key is not None:
-        filter_ = _resolve_condition_clause_to_q(conditions)
+    filter_ = _resolve_condition_clause_to_q(conditions)
     device_id_filter = Q()
     if device_ids:
         device_id_filter = Q(device_id__in=device_ids)
--- worker/notification/repository/device.py
+++ worker/notification/repository/device.py
@@ -7,14 +7,32 @@ from common.structure.condition import ConditionClause
 
 
 def _resolve_condition_clause_to_q(condition_clause: ConditionClause):
+    available_join_types = {'OR', 'AND'}
+    VALUE_INT = 'device_properties__value_int'
+    VALUE_STR = 'device_properties__value_str'
+    operators = {
+        'int_eq': VALUE_INT,
+        'int_gt': f'{VALUE_INT}__gt',
+        'int_gte': f'{VALUE_INT}__gte',
+        'int_lt': f'{VALUE_INT}__lt',
+        'int_lte': f'{VALUE_INT}__lte',
+        'str_exists': f'{VALUE_STR}__contains',
+        'str_eq': VALUE_STR,
+    }
+
     if condition_clause.conditions is not None:
+        if condition_clause.join_type not in available_join_types:
+            raise ValueError('Unknown join_type in ConditionClause')
         repeatable_clause = []
         for _condition_clause in condition_clause.conditions:
             repeatable_clause.append(_resolve_condition_clause_to_q(_condition_clause))
         return Q(*repeatable_clause, join_type=condition_clause.join_type)
+    if condition_clause.operator not in operators:
+        raise ValueError('Unknown operator in ConditionClause')
+
     parameter = {
         'device_properties__key': condition_clause.key,
-        'device_properties__value': condition_clause.value
+        operators[condition_clause.operator]: condition_clause.value
     }
     return Q(**parameter)
 
